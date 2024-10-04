0d4e9fa1963da6b3ffb625b4f721992cd7c5f185
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-10-04 14:14:10 +0530

    second commit

--- article-posting-app/src/Components/Navbar.jsx
+++ article-posting-app/src/Components/Navbar.jsx
@@ -5,12 +5,14 @@ function Navbar () {
         <nav className="w-full h-14 bg-indigo-200 flex justify-between
         px-4 md:px-4 items-center">
             <div className="text-2xl text-indigo-700 font-bold">Article</div>
-            <ul>
-                <li>Home</li>
-                <li>New Post</li>
-                <li>Profile</li>
+            <ul className="md:flex hidden font-semibold gap-4">
+                <li className = "max-[10px] cursor-pointer">Home</li>
+                <li className = "max-[10px] cursor-pointer">New Post</li>
+                <li className = "max-[10px] cursor-pointer">Profile</li>
             </ul>
-            <div>Login/Signup</div>
+            <div className="hidden md:block px-2 py-2 bg-indigo-700 text-white rounded font-bold cursor-pointer">
+                <button className="md:hidden">login/signup</button>
+            </div>
 
         </nav>
         </>
