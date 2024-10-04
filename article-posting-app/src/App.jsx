26c1df025a95cd88246945ef9e0f2c5295b46f4f
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-10-04 10:46:07 +0530

    first commit

---
+++ article-posting-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-cyan-200'>
+        <nav>
+          <li>Logo</li>
+          <ul>
+            <li>signup</li>
+            <li>login</li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
