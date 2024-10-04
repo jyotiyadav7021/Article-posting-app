014bbedd92624d349ba2dbcb710848902858890f
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-10-04 23:09:54 +0530

    5 commit

--- article-posting-app/src/App.jsx
+++ article-posting-app/src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Footer from './Components/Footer'
 import Navbar from './Components/Navbar'
 import Core from '././Components/Core'
+import Sidebar from './Components/Sidebar'
 
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
+      <Sidebar></Sidebar>
     
       <Core></Core>
       <Footer></Footer>
