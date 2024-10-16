169cd919c534a8a733249dfc6641ae243f1de5a1
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-10-16 11:16:09 +0530

    yes

--- article-posting-app/src/Components/Navbar.jsx
+++ article-posting-app/src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar () {
     </div>
     <ul className="flex items-center space-x-6">
       <li className="font-semibold text-gray-700">Home</li>
-      <li className="font-semibold text-gray-700">Articles</li>
+      <li className="font-semibold text-gray-700"> Your Articles</li>
       <li>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path d="M12 14l9-5-9-5-9 5 9 5z" />
